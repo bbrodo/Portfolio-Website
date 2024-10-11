e3caae2148aada94ea231a1912a28af8004db626
bbrodo <15292@coderacademy.edu.au>
2024-10-11 12:09:53 +1100

    feat: Added skills and experiences section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Navbar } from "./components/Navbar/Navbar";
 import styles from './App.module.css'
 import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
 
 function App() {
   return <div className={styles.app}>
     <Navbar />
     <Hero />
     <About />
+    <Experience />
   </div>
 }
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
    <section className={styles.container}>
     <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Brodey</h1>
-        <p className={styles.description}>I'm a full-stack developer with experience using React, NodeJS, Python and Psql. Reach out if you'd like to learn more!</p>
+        <p className={styles.description}>I'm a full-stack developer with experience using React, NodeJS, Flask and Psql. Reach out if you'd like to learn more!</p>
         <a href='mailto:brodey880@gmail.com' className={styles.contactBtn}>Contact Me</a>
     </div>
     <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg} />
