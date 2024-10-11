a5772efc70bfd8d962b47cb8fa8d3961efe0ff59
bbrodo <15292@coderacademy.edu.au>
2024-10-11 13:25:06 +1100

    feat: Contact section

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import styles from './Contact.module.css'
+import { getImageUrl } from '../../utils'
+
+export const Contact = () => {
+  return (
+    <footer id='contact' className={styles.container}>
+        <div className={styles.text}>
+            <h2>Contact</h2>
+            <p>Feel free to reach out!</p>
+        </div>
+        <ul className={styles.links}>
+            <li className={styles.link}>
+                <img src={getImageUrl('contact/emailIcon.png')} alt="email icon" />
+                <a href='mailto:brodey880@gmail.com'>brodey880@gmail.com</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl('contact/linkedinIcon.png')} alt="linkedin icon" />
+                <a href='https://https://www.linkedin.com/in/brodeybright/'>linkedin.com/brodeybright</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl('contact/githubIcon.png')} alt="github icon" />
+                <a href='https://github.com/bbrodo'>github.com/bbrodo</a>
+            </li>
+        </ul>
+    </footer>
+  )
+}
