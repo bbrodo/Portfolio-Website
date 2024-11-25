a82dc414aaa0942e0f57db89cbce5da9d2644587
bbrodo <15292@coderacademy.edu.au>
2024-11-25 15:00:03 +1100

    Updated Hero Image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ export const Hero = () => {
         <p className={styles.description}>I'm a full-stack developer with experience using React, NodeJS, Flask and Psql. Reach out if you'd like to learn more!</p>
         <a href='mailto:brodey880@gmail.com' className={styles.contactBtn}>Contact Me</a>
     </div>
-    <img src={getImageUrl("hero/heroImage.jpeg")} alt="Hero image of me" className={styles.heroImg} />
+    <img src={getImageUrl("hero/asdsdsds.jpg")} alt="Hero image of me" className={styles.heroImg} />
     <div className={styles.topBlur} />
     <div className={styles.bottomBlur} />
   </section>
